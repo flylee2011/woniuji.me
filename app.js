98ddc4e360cea6a982f0459323c828f939b12a02
liyifei <liyifei@le.com>
2017-06-09 16:41:12 +0800

    add code

---
+++ app.js
@@ -0,0 +1,63 @@
+/**
+ * @fileoverview 应用启动入口文件
+ * @author liyifei<yifei@zoocer.com>
+ * @date 2017/06
+ */
+// express
+var express = require('express');
+var bodyParser = require('body-parser');
+// 中间件
+var browserSync = require('browser-sync').create();
+// // webpack
+// var webpack = require('webpack');
+// var webpackConfig = require('./webpack.dev.js');
+// var webpackDevMiddleware = require('webpack-dev-middleware');
+// var webpackHotMiddleware = require('webpack-hot-middleware');
+// var webpackCompiler = webpack(webpackConfig);
+
+// 判断开发环境
+var env = process.argv[2] || process.env.NODE_ENV;
+var isDev = (env == 'develop') ? true : false;
+// 静态文件目录
+var staticDir = isDev ? './public/dev/' : './public/dist/';
+
+var app = express();
+
+// 中间件
+if (isDev) {
+    // // webpack 开发环境
+    // app.use(webpackDevMiddleware(webpackCompiler, {
+    //     publicPath: webpackConfig.output.publicPath,
+    //     noInfo: true
+    // }));
+    // // webpack 热替换
+    // app.use(webpackHotMiddleware(webpackCompiler));
+}
+// 解析 body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// 路由
+// 静态资源
+app.use('/', express.static(staticDir));
+
+// 启动
+if (isDev) {
+    app.listen(8001, function() {
+        // browserSync.init({
+        //     open: false,
+        //     // ui: false,
+        //     notify: false,
+        //     proxy: 'localhost:8001',
+        //     files: [staticDir + '*.html'],
+        //     port: 8002
+        // });
+        console.log('Running on Development...');
+    });
+} else {
+    app.listen(8001, function() {
+        console.log('Running on Production...');
+    });
+}
